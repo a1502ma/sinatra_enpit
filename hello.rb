8fb75e6de9746b7042aaec270d78c818cb905060
None <a1502ma@aiit.ac.jp>
2015-09-23 05:07:11 +0000

    Add settings for Heroku

--- hello.rb
+++ hello.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'date'
 
 get '/' do
-  "Hello World!"
+  "Hello World!<br>" +
+  "<a href='/today'>today</a><br>" +
+  "<a href='/now'>now</a>"
+end
+
+get '/today' do
+  Date.today.strftime("%F") + "<br>" +
+  "<a href='/'>TOP</a>"
+end
+
+get '/now' do
+  Time.now.strftime("%T") + "<br>" +
+  "<a href='/'>TOP</a>"
 end
